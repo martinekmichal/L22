5f8b17c5a2b678fe08aabc1a4c2de996c91522fc
martinekmichal <martinek.m.84@gmail.com>
2024-09-05 19:12:46 +0200

    models

--- my_first_web/members/views.py
+++ my_first_web/members/views.py
@@ -5,6 +5,10 @@ from django.template import loader
 def members_old(request):
     return HttpResponse("Hello world!")
 
-def members(request):
+def members_old2(request):
   template = loader.get_template('index.html')
   return HttpResponse(template.render())
+
+def members(request):
+  template = loader.get_template('index.html')
+  return render(request, "index.html")
