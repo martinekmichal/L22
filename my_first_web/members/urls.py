9649cdc460dd3bbf924e9fcd278354726e244b5f
martinekmichal <martinek.m.84@gmail.com>
2024-09-05 20:31:04 +0200

    details - back
    <a href / to back>

--- my_first_web/members/urls.py
+++ my_first_web/members/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('members/', views.members, name='members'),
-
+    path('all_members/', views.all_members, name='all_members'),
+    path('all_members/details/<int:id>', views.details, name='details'),
 ]
--- my_first_web/members/views.py
+++ my_first_web/members/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
+
+
 
 def members_old(request):
     return HttpResponse("Hello world!")
@@ -12,3 +15,14 @@ def members_old2(request):
 def members(request):
   template = loader.get_template('index.html')
   return render(request, "index.html")
+
+def all_members(request):
+  mymembers = Member.objects.all().values()
+  context = {
+    'mymem': mymembers,
+  }
+  return render(request, "all_members.html", context)
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  return render(request, "detail.html", {"mymember":mymember})
