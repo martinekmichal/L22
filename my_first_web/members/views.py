8a921efbad8139e212fe9aafc9635121ee3ac81f
martinekmichal <martinek.m.84@gmail.com>
2024-09-05 18:53:16 +0200

    index.html

--- my_first_web/members/views.py
+++ my_first_web/members/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+def members_old(request):
+    return HttpResponse("Hello world!")
 
 def members(request):
-    return HttpResponse("Hello world!")
-
-def my_first(request):
-    return HttpResponse("Hello world!")
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
